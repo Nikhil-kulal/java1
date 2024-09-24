9d6709239e4f4545b199e69284007188b9c6de08
Nikhil-kulal <nikhil.23cs083@sode-edu.in>
2024-09-24 08:04:07 +0530

    Add files via upload

---
+++ MatrixAddition_1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class MatrixAddition_1{
+    public static void main(String[]args){
+        int N=Integer.parseInt(args[0]);
+        int[][] matrix1=new int[N][N];
+        int[][] matrix1=new int[N][N];
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter the elements of first matrix :");
+        for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+          matrix1[i][j]=scanner.nextInt();}}
+for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+          System.out.print(matrix1[i][j]+" ");}
+System.out.println();}
+
+
+}}
+         
